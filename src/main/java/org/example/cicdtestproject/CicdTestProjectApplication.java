76ac797e448e85a38b6f40f30e6695c7b64d4d3a
JungMyung Jang <140879889+jungmyung16@users.noreply.github.com>
2025-05-07 10:21:06 +0900

    first commit

---
+++ src/main/java/org/example/cicdtestproject/CicdTestProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.cicdtestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdTestProjectApplication.class, args);
+    }
+
+}
